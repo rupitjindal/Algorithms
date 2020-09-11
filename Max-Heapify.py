9437c8397fa8168ffb958e2e957812b52a3ccb7e
rupitjindal <67161816+rupitjindal@users.noreply.github.com>
2020-09-11 13:13:16 +0530

    Update Max-Heapify.py
    Building Max-Heap...

--- Max-Heapify.py
+++ Max-Heapify.py
@@ -1,22 +1,28 @@
-# sorting heap from index specified by input
+#Building Heap and sorting it
+def Build_Max_Heap(A):
+    heap_size = len(A)
+    for i in range(int((len(A)-1)/2),0,-1):
+        print(i)
+        Max_Heapify(A,i)
+    return A
+# Sorting heap
 def Max_Heapify(A,i):
     l = 2*i
     r = l+1
-    if l<len(A) and A[l] > A[i]:
+    if l < len(A) and A[l] > A[i]:
         largest = l
     else:
         largest = i
-    if r<len(A) and A[r] > A[largest]:
+    if r < len(A) and A[r] > A[largest]:
         largest = r
-    if largest !=i:
+    if largest != i:
         swap = A[i]
         A[i] = A[largest]
         A[largest] = swap
         Max_Heapify(A,largest)
     return A
+#Ener array
+A = [int(x) for x in input("Enter Array value with first element as 0 ").split()]    
+print(Build_Max_Heap(A))
 
 
-
-A = [int(x) for x in input("Enter Array value with first element as 0 ").split()]
-index = int(input("Enter the root  of subtree to be sorted  ")) #Enter the root value of subtree to give its proper position
-print(Max_Heapify(A,index))
