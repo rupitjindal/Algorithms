40d835d8e28f0a06c916cb650b1d8350bda00eef
rupitjindal <67161816+rupitjindal@users.noreply.github.com>
2020-09-12 14:40:30 +0530

    Update Max-Heapify.py

--- Max-Heapify.py
+++ Max-Heapify.py
@@ -1,28 +1,35 @@
-#Building Heap and sorting it
-def Build_Max_Heap(A):
-    heap_size = len(A)
-    for i in range(int((len(A)-1)/2),0,-1):
-        print(i)
-        Max_Heapify(A,i)
+#Sorting Heap
+def Heapsort(A):
+    Build_Max_Heap(A)
+    a = len(A)
+    for i in range(a-1,1,-1):
+            swap = A[1]
+            A[1] = A[i]
+            A[i] = swap
+            Max_Heapify(A,1,i)
     return A
-# Sorting heap
-def Max_Heapify(A,i):
+
+#Building Heap
+def Build_Max_Heap(A):
+        for i in range(int(((len(A)-1)/2)),0,-1):
+            Max_Heapify(A,i,len(A))
+        return A
+#Maintaining Heap-Max property
+def Max_Heapify(A,i,a):
     l = 2*i
     r = l+1
-    if l < len(A) and A[l] > A[i]:
-        largest = l
+    if l< a and A[l] > A[i]:
+        largest =  l
     else:
         largest = i
-    if r < len(A) and A[r] > A[largest]:
+    if r<a and A[r] > A[largest]:
         largest = r
-    if largest != i:
+    if largest!= i:
         swap = A[i]
         A[i] = A[largest]
         A[largest] = swap
-        Max_Heapify(A,largest)
+        Max_Heapify(A,largest,a)
     return A
-#Ener array
-A = [int(x) for x in input("Enter Array value with first element as 0 ").split()]    
-print(Build_Max_Heap(A))
 
-
+A  = [int(x) for x in input("Enter array to be sorted starting with 0 ").split()]
+print(Heapsort(A))
